be0027cad7f6830f21d15848117400f023ac456e
Akzhol <akjolasan75@gmail.com>
2025-11-03 18:36:53 +0600

    DZ3

--- js/lesson.js
+++ js/lesson.js
@@ -13,3 +13,56 @@ phoneButton.onclick = () => {
     phoneResult.style.color = "RED";
   }
 };
+
+//lesson 3 TAB SLIDER
+//HOMEWORK 3
+
+const tabsParent = document.querySelector(".tab_content_items");
+const tabContentBlocks = document.querySelectorAll(".tab_content_block");
+const tabs = document.querySelectorAll(".tab_content_item");
+
+const hideTabContent = () => {
+  tabContentBlocks.forEach((block) => {
+    block.style.display = "none";
+  });
+  tabs.forEach((tab) => {
+    tab.classList.remove("tab_content_item_active");
+  });
+};
+
+const showTabContent = (blockIndex = 0) => {
+  tabContentBlocks[blockIndex].style.display = "block";
+  tabs[blockIndex].classList.add("tab_content_item_active");
+};
+
+let currentIndex = 0;
+hideTabContent();
+showTabContent(currentIndex);
+
+//HOMEWORK 3
+//AUTO TABS SWITCH EVERY 3 SEC
+let autoSwitch; // timer
+
+const tabsSwitch = () => {
+  clearInterval(autoSwitch); // clear old timer
+  autoSwitch = setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= tabs.length) currentIndex = 0;
+    hideTabContent();
+    showTabContent(currentIndex);
+  }, 3000);
+};
+tabsSwitch();
+
+tabsParent.onclick = (event) => {
+  if (event.target.classList.contains("tab_content_item")) {
+    tabs.forEach((tab, index) => {
+      if (event.target === tab) {
+        currentIndex = index; //update index on clicked
+        hideTabContent();
+        showTabContent(index);
+        tabsSwitch(); //restart
+      }
+    });
+  }
+};
